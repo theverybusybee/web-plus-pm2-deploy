7f6add48ad96d7c33e54e5de2a27e143787f82e9
theverybusybee <v_antonchik@mail.ru>
2023-09-27 14:27:58 +0300

    router crash test added

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -11,6 +11,13 @@ import {
 import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
 
 const router = Router();
+
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
 
