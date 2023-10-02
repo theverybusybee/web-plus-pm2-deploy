03df804361fb04d83e2fe5b34f5ede1d832cea75
theverybusybee <v_antonchik@mail.ru>
2023-10-03 01:49:02 +0300

    some changes added to ecosystem file

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       path: DEPLOY_PATH,
       "pre-deploy-local": `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
       "post-deploy":
-        "cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+        "cd ./current/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
     },
   },
 };
