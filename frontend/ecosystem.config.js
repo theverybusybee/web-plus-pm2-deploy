56fc9f5f12c5d275a501cee66681aead320bf169
theverybusybee <v_antonchik@mail.ru>
2023-10-03 01:50:19 +0300

    small fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "post-deploy": "cd current/frontend && npm i && npm run build",
+      "post-deploy": "cd ./current/frontend && npm i && npm run build",
     },
   },
 };
