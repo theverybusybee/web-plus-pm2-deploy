e267d1fedca981e4e58207fa12e83beb6925caa1
theverybusybee <v_antonchik@mail.ru>
2023-10-03 16:02:47 +0300

    test change

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "post-deploy": `cd ${DEPLOY_PATH}/frontend && npm i && npm run build`,
+      "post-deploy": `cd ~/testFolder/current/frontend && npm i && npm run build`,
     },
   },
 };
